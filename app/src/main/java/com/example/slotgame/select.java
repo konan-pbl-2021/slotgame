470c4743ea947dc7d61b0a7d1095fa5b781c25ee
haruyang400 <haruyaki@icloud.com>
2021-11-19 15:28:56 +0900

    一枚目 二枚目 難易度別3ページ 四枚目のhiroは仮 hiroは完成しだい撤去予定

--- app/src/main/java/com/example/slotgame/select.java
+++ app/src/main/java/com/example/slotgame/select.java
@@ -2,7 +2,10 @@ package com.example.slotgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class select extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class select extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select);
+        Button select1Button = (Button)findViewById(R.id.select1);
+        select1Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(select.this, kantan.class);
+                startActivity(intent);
+            }
+        });
+        Button select2Button = (Button)findViewById(R.id.select2);
+        select2Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(select.this, hutu.class);
+                startActivity(intent);
+            }
+        });
+        Button select3Button = (Button)findViewById(R.id.select3);
+        select3Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(select.this, muzukashi.class);
+                startActivity(intent);
+            }
+        });
     }
 }
