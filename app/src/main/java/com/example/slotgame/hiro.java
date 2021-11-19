262afff7e55e2b26a85b0b48d32c91dedb7b1b06
haruyang400 <haruyaki@icloud.com>
2021-11-19 16:58:16 +0900

    一枚目 二枚目 難易度別3ページ 四枚目のhiroは仮 hiroは完成しだい撤去予定

--- app/src/main/java/com/example/slotgame/hiro.java
+++ app/src/main/java/com/example/slotgame/hiro.java
@@ -10,5 +10,6 @@ public class hiro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiro);
+
     }
 }
