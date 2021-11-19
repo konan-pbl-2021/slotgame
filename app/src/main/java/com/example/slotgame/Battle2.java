6e44057f6b2df12a442d038313398cd814165d2b
Hide-no-Aki <huangdouxiuping139@gmail.com>
2021-11-19 15:24:22 +0900

    イラスト

---
+++ app/src/main/java/com/example/slotgame/Battle2.java
@@ -0,0 +1,14 @@
+package com.example.slotgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Battle2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle2);
+    }
+}
---
+++ app/src/main/java/com/example/slotgame/DeadView.java
@@ -0,0 +1,14 @@
+package com.example.slotgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeadView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dead_view);
+    }
+}
